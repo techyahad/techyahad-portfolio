244af1640eaad3d8fc449e2ca1b301f4df066388
Abdul Ahad <130489838+AAhad-Dev@users.noreply.github.com>
2024-09-22 17:18:05 +0500

    Updates

--- src/Components/Sections/Portfolio/Portfolio.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Portfolio: React.FC = () => {
-    return (
-        <div>What I've been working on so far</div>
-    )
-}
-
-export default Portfolio
--- src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-declare module "*.gif" {
-  const src: string;
-  export default src;
-}
